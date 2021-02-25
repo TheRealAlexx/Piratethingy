2c0a3ac048db2653f328bfb96fe73df1e4556837
TheRealAlexx <78887388+TheRealAlexx@users.noreply.github.com>
2021-02-25 11:53:42 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+import random
+#Random Numbers just to test my list "Skills"
+
+pirateboat = [8, 36, 45, 68]
+
+teavessel = [10, 40, 64]
+
+#Training if's and elif's while using the "random" module.
+
+if random.choice(teavessel) > random.choice(pirateboat):
+    print("The tea vessel sucessfully escaped the pirates!")
+
+elif random.choice(pirateboat) > random.choice(teavessel):
+    print("The pirates captured the tea shipment!")
+
+
