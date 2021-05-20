8c24e4f7517c8ae7806d60ac89683bc72a923beb
Alex <78887388+TheRealAlexx@users.noreply.github.com>
2021-05-20 15:33:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import random
-#Random Numbers just to test my list "Skills"
 
 pirateboat = [8, 36, 45, 68]
 
 teavessel = [10, 40, 64]
-
-#Training if's and elif's while using the "random" module.
 
 if random.choice(teavessel) > random.choice(pirateboat):
     print("The tea vessel sucessfully escaped the pirates!")
